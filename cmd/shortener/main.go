40cb4df63597132269341eed84aa9e80eb6dc01f
IamNotUrKitty <afonushkin33@gmail.com>
2024-04-03 10:16:05 +0300

    iter1 shrtener

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,81 @@
 package main
 
-func main() {}
+import (
+	"encoding/binary"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/sqids/sqids-go"
+)
+
+func makeMainHandler() http.HandlerFunc {
+	urls := make(map[string]string)
+	s, _ := sqids.New()
+
+	return func(res http.ResponseWriter, req *http.Request) {
+
+		if req.Method == http.MethodPost {
+			if req.Header.Get("Content-type") != "text/plain" {
+				res.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			body, errBody := io.ReadAll(req.Body)
+
+			if errBody != nil {
+				res.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			_, errUrl := url.ParseRequestURI(string(body))
+
+			if errUrl != nil {
+				res.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			defer req.Body.Close()
+
+			// len < 7 problems
+			data := binary.BigEndian.Uint64(body)
+
+			hash, _ := s.Encode([]uint64{data})
+
+			urls[hash] = string(body)
+
+			res.WriteHeader(http.StatusCreated)
+
+			res.Write([]byte("http://localhost:8080/" + hash))
+
+			return
+		}
+
+		if req.Method == http.MethodGet {
+			hash := strings.TrimPrefix(req.URL.Path, "/")
+			url, val := urls[hash]
+			if val {
+				http.Redirect(res, req, url, http.StatusTemporaryRedirect)
+			} else {
+				res.WriteHeader(http.StatusBadRequest)
+			}
+
+			return
+		}
+
+		res.WriteHeader(http.StatusBadRequest)
+
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", makeMainHandler())
+
+	err := http.ListenAndServe(":8080", mux)
+
+	if err != nil {
+		panic(err)
+	}
+}
